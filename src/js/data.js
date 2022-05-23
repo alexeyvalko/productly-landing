373b3218d12577e8199a3adace6a94bace286d28
Alexey Valko <valko.alexey@gmail.com>
2022-05-23 22:54:10 +0300

    fix: fix data

--- src/js/data.js
+++ src/js/data.js
@@ -3,7 +3,7 @@ export const data = [
     id: '1',
     title:
       'Increasing Prosperity With Positive Thinking',
-    img_url: '../assets/img/strategies/1.jpg',
+    img_url: './assets/img/strategies/1.jpg',
     tags: ['Culture', 'Art'],
     content:
       'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
@@ -12,7 +12,7 @@ export const data = [
   {
     id: '2',
     title: 'Motivation Is The First Step To Success',
-    img_url: '../assets/img/strategies/2.jpg',
+    img_url: './assets/img/strategies/2.jpg',
     tags: ['Culture', 'Art'],
     content:
       'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
@@ -21,7 +21,7 @@ export const data = [
   {
     id: '3',
     title: 'Success Steps For Your Personal Or Business Life',
-    img_url: '../assets/img/strategies/3.jpg',
+    img_url: './assets/img/strategies/3.jpg',
     tags: ['Culture', 'Art'],
     content:
       'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
@@ -31,7 +31,7 @@ export const data = [
     id: '4',
     title:
       'Increasing Prosperity With Positive Thinking',
-    img_url: '../assets/img/strategies/4.jpg',
+    img_url: './assets/img/strategies/4.jpg',
     tags: ['Culture', 'Art'],
     content:
       'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
@@ -40,7 +40,7 @@ export const data = [
   {
     id: '5',
     title: 'Motivation Is The First Step To Success',
-    img_url: '../assets/img/strategies/5.jpg',
+    img_url: './assets/img/strategies/5.jpg',
     tags: ['Culture', 'Art'],
     content:
       'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
@@ -50,7 +50,7 @@ export const data = [
     id: '1',
     title:
       'Increasing Prosperity With Positive Thinking',
-    img_url: '../assets/img/strategies/1.jpg',
+    img_url: './assets/img/strategies/1.jpg',
     tags: ['Culture', 'Art'],
     content:
       'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
@@ -59,7 +59,7 @@ export const data = [
   {
     id: '2',
     title: 'Motivation Is The First Step To Success',
-    img_url: '../assets/img/strategies/2.jpg',
+    img_url: './assets/img/strategies/2.jpg',
     tags: ['Culture', 'Art'],
     content:
       'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
@@ -68,7 +68,7 @@ export const data = [
   {
     id: '3',
     title: 'Success Steps For Your Personal Or Business Life',
-    img_url: '../assets/img/strategies/3.jpg',
+    img_url: './assets/img/strategies/3.jpg',
     tags: ['Culture', 'Art'],
     content:
       'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
